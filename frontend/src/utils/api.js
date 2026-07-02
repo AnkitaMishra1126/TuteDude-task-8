8ce44a77aa0b5bbe973a533604a117c2764b5e59
AnkitaMishra1126 <its.ankita.mishra26@gmail.com>
2026-07-02 18:31:28 +0530

    Update API base URL for axios instance

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
   
 const api =  axios.create({
-  baseURL: "https://tutedude-task-8.onrender.com/api/v2",
+  baseURL: "https://tutedude-task-8.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },

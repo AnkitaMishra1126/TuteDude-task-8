7cbd4bdb0ff96daf90bcd711240f6ca726a638d3
AnkitaMishra1126 <its.ankita.mishra26@gmail.com>
2026-07-03 21:27:02 +0530

    Update API endpoints to version 2

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -1,26 +1,26 @@
 import api from "../utils/api";
 //get all tasks
 export const getTasks = async (email)=>{
-    const response = await api.get(`/getlist?email=${email}`);
+    const response = await api.get(`/api/v2/getlist?email=${email}`);
     return response.data;
 };
 //add tasks
 export const addTask = async (taskData) => {
-    const response = await api.post("/addlist", taskData);
+    const response = await api.post("/api/v2/addlist", taskData);
     return response.data;
 };
 //update tasks
 export const updateTask = async (id,taskData) => {
-    const response = await api.put(`/updatelist/${id}`, taskData);
+    const response = await api.put(`/api/v2/updatelist/${id}`, taskData);
     return response.data;
 };
 //delete tasks
 export const deleteTask = async (id, email) => {
-    const response = await api.delete( `/deletelist/${id}?email=${email}`);
+    const response = await api.delete( `/api/v2/deletelist/${id}?email=${email}`);
     return response.data;
 };
 //search task
 export const searchTask = async (keyword) => {
-    const response = await api.get( `/searchlist?keyword=${keyword}`);
+    const response = await api.get( `/api/v2/searchlist?keyword=${keyword}`);
     return response.data;
 };

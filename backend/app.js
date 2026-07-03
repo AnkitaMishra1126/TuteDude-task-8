d441558b16f7f74d1ff621e7d621f28ac238fc86
AnkitaMishra1126 <its.ankita.mishra26@gmail.com>
2026-07-03 20:20:49 +0530

    Update CORS origin for deployment URL

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(cors(
   {
     origin:[
       "http://localhost:5173",
-      "https://tutedude-task-8.netlify.app",
+      "https://famous-donut-f3c594.netlify.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization","id"],

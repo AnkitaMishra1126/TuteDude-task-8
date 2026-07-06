407177ea75109338eab8e3e3dff09ef1e237506c
AnkitaMishra1126 <its.ankita.mishra26@gmail.com>
2026-07-06 11:59:46 +0530

    Add updateStatus route to list.js

--- backend/route/list.js
+++ backend/route/list.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import {addList, updateList, deleteList , getList , searchList} from '../controllers/listController.js';
+import {addList, updateList, deleteList , getList , searchList,updateStatus} from '../controllers/listController.js';
 
 const router = express.Router();
 router.post("/addlist", addList);
@@ -8,4 +8,5 @@ router.put("/updatelist/:id", updateList);
 router.delete("/deletelist/:id", deleteList);
 router.get("/getlist", getList);
 router.get("/searchlist", searchList);
+router.put("/updatestatus/:id", updateStatus);
 export default router;
